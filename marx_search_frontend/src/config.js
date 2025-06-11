f6f635f0761417bff48c0581490dbc1d29ee3901
Zachary Sedefian <zbsedefian@gmail.com>
2025-06-10 21:11:07 -0400

    use https and delete trailing ;

--- marx_search_frontend/src/config.js
+++ marx_search_frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "http://api.marxsearch.org;";
+    : "https://api.marxsearch.org";
 
 export default API_BASE_URL;
