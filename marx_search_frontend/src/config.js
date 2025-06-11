6c359912385f1521ea113076c45c3655eece8ce7
Zachary Sedefian <zbsedefian@gmail.com>
2025-06-10 20:58:38 -0400

    Use api.marxsearch.org

--- marx_search_frontend/src/config.js
+++ marx_search_frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000"
-    : "http://3.142.200.52";
+    : "http://api.marxsearch.org;";
 
 export default API_BASE_URL;
